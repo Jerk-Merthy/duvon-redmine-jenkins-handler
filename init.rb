46c59c697cc2c3841f08d8faf8e8d2d282d7ffc8
root <root@loner.zencoo.com>
2017-12-13 06:21:37 -0500

    add

--- init.rb
+++ init.rb
@@ -1,34 +1,18 @@
 require 'redmine'
-require_dependency 'rjc/hooks/hooks'
+require_dependency 'rrac/hooks/hooks'
 
-Redmine::Plugin.register :rjc do
-    name 'Remote Jenkins Call'
-    author 'Paul Tymoshenko'
-    description 'This plugin for Redmine to Jenkins integration'
+Redmine::Plugin.register :rrac do
+    name 'rrac'
+    author 'Jerk Merthy'
+    description 'This plugin for Redmine to any service with REST API'
     version '0.0.1'
-    #url ''
-    author_url 'https://github.com/Pepperrs/red_jenkins'
+    url 'https://github.com/Jerk-Merthy/rrac.git'
+    author_url 'https://github.com/Jerk-Merthy'
 
     settings :default => {
-        'rjc_server_url' => nil,
-        'rjc_server_ip' => nil,
-        'rjc_server_port' => nil,
-        'rjc_proxy_ip' => nil,
-        'rjc_proxy_port' => nil,
-        'rjc_jenkins_path' => "/",
-        'rjc_username' => nil,
-        'rjc_password' => nil,
-        'rjc_password_base64' => nil,
-        'rjc_log_location' => nil,
-        'rjc_log_level' => 1,
-        'rjc_timeout' => 120,
-        'rjc_http_open_timeout' => 120,
-        'rjc_http_read_timeout' => 120,
-        'rjc_ssl' => false,
-        'rjc_follow_redirects' => false,
-        'rjc_identity_file' => nil,
-        'rjc_cookies' => nil
+        'rrac_resources' => nil,
+	'rrac_actions' => nil
     },
-        :partial => 'settings/rjc_settings'
+        :partial => 'settings/rrac_settings'
 
 end
