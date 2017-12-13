1691fe8fdceb977059a2942540792855043c508c
paul.tymoshenko <pavlo.tymoshenko@stoamigo.com>
2017-12-13 14:00:41 +0200

    no message

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 require_dependency 'rrac/hooks/hooks'
 
 Redmine::Plugin.register :rrac do
-    name 'rrac'
+    name 'RRAC'
     author 'Jerk Merthy'
     description 'This plugin for Redmine integration with any service having REST API'
     version '0.0.1'
@@ -10,8 +10,8 @@ Redmine::Plugin.register :rrac do
     author_url 'https://github.com/Jerk-Merthy'
 
     settings :default => {
-        'rrac_resources' => nil,
-        'rrac_actions' => nil
+        'rrac_resources' => '', 
+        'rrac_actions' => ''
     },
         :partial => 'settings/rrac_settings'
 end
