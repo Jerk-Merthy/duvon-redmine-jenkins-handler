feda66ea35399672d3be2cf645b3650b7f16941f
paul.tymoshenko <pavlo.tymoshenko@stoamigo.com>
2017-12-13 13:51:47 +0200

    \t to spaces

--- app/controllers/rrac_controller.rb
+++ app/controllers/rrac_controller.rb
@@ -1,9 +1,9 @@
 class JenkinshandlerController < ApplicationController
-	def initialize
-	    @rrac_settings =  {}
-	    if Setting.plugin_rrac['rrac_resources'].to_s == ""
+    def initialize
+        @rrac_settings =  {}
+        if Setting.plugin_rrac['rrac_resources'].to_s == ""
             @rrac_settings[:resources] = nil
-	    else
+        else
             # transform to array of turples
             @rrac_settings[:resources] = Setting.plugin_rrac['rrac_resources'].to_s
         end
@@ -14,5 +14,5 @@ class JenkinshandlerController < ApplicationController
             # transform to array of turples
         @rrac_settings[:actions] = Setting.plugin_rrac['rrac_actions'].to_s
         end
-	end
+    end
 end
